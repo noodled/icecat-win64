0fa722846f154d00082365430447c3bc7e557038
user <user@user.com>
2019-05-08 20:50:28 +0600

    60.6.2

--- for_installer/build.bat
+++
@@ -1,1 +0,0 @@
-copy /b 7zSD.sfx.exe + app.tag + archive.7z icecat-60.6.1.en-US.win64.installer.exe
---
+++ misc/installer/build.bat
@@ -0,0 +1,1 @@
+copy /b 7zSD.sfx.exe + app.tag + archive.7z icecat-60.6.2.en-US.win64.installer.exe
